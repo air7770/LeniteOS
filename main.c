2a9266cc1043b3a690cd72c547761852992bb25a
airs7770 <gureevalex12@gmail.com>
2026-05-07 20:01:24 +0300

    New version! 0.0.2 beta

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+
 #define VMEM 0xB8000 // video code
 #define WIGTH 80
 #define HEIGHT 25
@@ -23,13 +24,13 @@ unsigned char inb(unsigned short port) {
     return result;
 }
 
-// variable for os, it's var need's for video and cursor
+// Переменные
 char* VIDEO = (char*) VMEM;
 int cursor_x = 0;
 int cursor_y = 0;
 char input_buffer[MAX_BUFFER];
 int index_buffer = 0;
-
+// Сама клавиатура
 char keyboard_map[128] = {
     0,
     27,
@@ -53,7 +54,7 @@ char keyboard_map[128] = {
     0,
     ' ',
 };
-
+// Считавание клавиатуры
 char keyboard_gc() {
     static unsigned char last = 0;
     unsigned char scancode;
@@ -77,7 +78,7 @@ char keyboard_gc() {
 
 
 }
-/*
+/* 
 void put_char(char c) {
     if (c == '\n') {
         cursor_x = 0;
@@ -87,6 +88,17 @@ void put_char(char c) {
     
 }
 */
+
+int strcmp(const char* a, const char* b) {
+    int i = 0;
+
+    while (a[i] && b[i]) {
+        if (a[i] != b[i]) return 0;
+        i++;
+    }
+    return a[i] == b[i];
+}
+// считавание символа
 void put_char(char c) {
     if (c == '\n') {
         cursor_x = 0;
@@ -105,45 +117,77 @@ void put_char(char c) {
         cursor_x = 0;
         cursor_y++;
     }
+    
 }
-
+// Добавление символа 
 void add_char(char C) {
     if(index_buffer < MAX_BUFFER - 1) {
         input_buffer[index_buffer++] = C;
     }
 }
-
+// Функция сложения всех символов
 void write(const char* str) {
     for (int i = 0; str[i] != '\0'; i++) {
-	put_char(str[i]);
+	    put_char(str[i]);
     }
 }
-
+// Очистка экрана
 void clear_scr() {
     for (int i = 0; i < WIGTH * HEIGHT * 2; i += 2) {
 	VIDEO[i] = ' ';
  	VIDEO[i + 1] = 0x07;
     }
 }
+// Функция echo
+void cmd_echo() {
+    write("echo: ");
+    write("\n");
+}
+// Функция clear
+void cmd_clear() {
+    clear_scr();
+    write("> ");
+}
+// Функция help
+void cmd_help() {
+    write("Commands:\n");
+    write("help   - show commands\n");
+    write("clear  - clear screen\n");
+    write("echo   - repeat text\n");
+}
 
+// Ввод команды
 void enter_command() {
     input_buffer[index_buffer] = '\0';
     
-    write("\nTEST- ");
     write(input_buffer);
     write("\n");
+    if (strcmp(input_buffer, "help")) {
+        cmd_help();
+    }
+    else if (strcmp(input_buffer, "clear")) {
+        cmd_clear();
+    }
+    else if (strcmp(input_buffer, "echo")) {
+        cmd_echo();
+    }
+    else {
+        write("Command not found");
+    }
+
 
     index_buffer = 0;
 }
 
+
+
+
+// Основная функция
 void kernel_main_os() {
 
     clear_scr();
     
-    write("HELLO, THIS IS MY 1 REAL KERNEL!!!");
-    
-
-    
+    write("LeniteOS kernel is start!\n\n");
 
     while (1) {
         char ckey = keyboard_gc();
@@ -154,6 +198,7 @@ void kernel_main_os() {
             enter_command();
             write("> ");
         }   
+        
         else {
             put_char(ckey);
             add_char(ckey);
